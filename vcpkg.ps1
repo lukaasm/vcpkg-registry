62caedfa548996f50b62a2a940122b3d926f0fa8
lukaasm <lukaasm@gmail.com>
2025-06-28 11:49:43 +0200

    - update helper script

--- vcpkg.ps1
+++ vcpkg.ps1
@@ -1,6 +1,9 @@
 param( $action, $port_name, $features )
 
 if ( $action -eq "install" ) {
+    if ( [string]::IsNullOrEmpty($features) ) {
+        $features = "core"
+    }
     & "$Env:VCPKG_ROOT\vcpkg.exe" remove --overlay-ports=./ports $port_name
     & "$Env:VCPKG_ROOT\vcpkg.exe" install --overlay-ports=./ports "$port_name[$features]" --editable
 }
