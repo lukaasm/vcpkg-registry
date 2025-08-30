1a903c1c6f874c6bd8ec7c51771d2491b8a052a1
lukaasm <lukaasm@gmail.com>
2025-08-30 18:59:46 +0200

    - vcpkg.ps1: update helper script

--- vcpkg.ps1
+++ vcpkg.ps1
@@ -17,3 +17,15 @@ if ( $action -eq "remove" ) {
     & "$Env:VCPKG_ROOT\vcpkg.exe" remove --overlay-ports=./ports $port_name --triplet=x64-windows
     & "$Env:VCPKG_ROOT\vcpkg.exe" remove --overlay-ports=./ports $port_name --triplet=x64-windows-static-md
 }
+
+if ( $action -eq "update" ) {
+    & "$Env:VCPKG_ROOT\vcpkg.exe" format-manifest "ports/nri/vcpkg.json"
+
+    git add ports/$port_name
+    git commit -m "- ${port_name}: update port"
+
+    & "$Env:VCPKG_ROOT\vcpkg.exe" x-add-version --x-builtin-ports-root=./ports --x-builtin-registry-versions-dir=./versions $port_name
+
+    git add versions
+    git commit -m "- ${port_name}: update version"
+}
