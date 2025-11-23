c9185dfef864ce388cee07db52d2e9c4da39e40a
lukaasm <lukaasm@gmail.com>
2025-11-23 16:44:07 +0100

    - ned-embed: update port

---
+++ ports/ned-embed/portfile.cmake
@@ -0,0 +1,28 @@
+if (EXISTS "${CMAKE_CURRENT_LIST_DIR}/source")
+    set( SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/source")
+else()
+    vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO lukaasm/imgui-ned-embed
+        REF adc6da70d3a1f9f7f5f18b807f297e446b63a028
+        SHA512 adc6da70d3a1f9f7f5f18b807f297e446b63a028
+        HEAD_REF cleanup
+    )
+endif()
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED_LIBS )
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
+        -DUSE_BUNDLED_ZLIB=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "ned")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
